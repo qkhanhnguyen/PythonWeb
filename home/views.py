4c46a62d240418f1a3038ff92550297675a4096b
qkhanhnguyen <nguyenquockhanh13488@gmail.com>
2020-05-19 23:05:51 +0700

    commit app home

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    response = HttpResponse()
+    response.writelines("<h1>Welcome</h1>")
+    response.write("This is APP HOME")
+    return response
