8cea348aa60b9ee358a968466a57d5d039e72b74
qkhanhnguyen <nguyenquockhanh13488@gmail.com>
2020-06-07 19:49:43 +0700

    blog view

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
